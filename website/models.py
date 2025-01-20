51a66f6483fbe55d6a2a50ca8feace5f59996a3e
xczheng <zhengxc105@gmail.com>
2025-01-21 04:00:56 +0800

    Add functionatliy for adding offers

--- website/models.py
+++ website/models.py
@@ -6,11 +6,12 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(100))
+    contactinfo = db.Column(db.String(1000))
     offers = db.relationship('Offer')
 
 class Offer(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    bookname = db.Column(db.String(10000))
+    title = db.Column(db.String(10000))
+    author = db.Column(db.String(10000))
     date = db.Column(db.DateTime(timezone=True), default=func.now())
     userid = db.Column(db.Integer, db.ForeignKey('user.id'))
-
--- website/views.py
+++ website/views.py
@@ -1,10 +1,40 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect, flash, url_for
 from flask_login import login_required, current_user
+from .models import Offer
+from .models import db
+from sqlalchemy import text
 
 views = Blueprint('views', __name__)
 
 @views.route('/', methods=['GET'])
 @login_required
 def home():
-    return render_template("index.html", user=current_user)
+    rows = db.session.execute(text('SELECT offer.title, offer.author, user.username FROM offer JOIN user ON user.id=offer.userid WHERE offer.userid != :uid'), {'uid': current_user.id})
+    return render_template("index.html", user=current_user, rows=rows)
+
+@views.route('/add', methods=['GET', 'POST'])
+@login_required
+def add():
+    if request.method == 'POST':
+        # Add the offer
+        title = request.form.get('title')
+        author = request.form.get('author')
+        userid = current_user.id
+
+        if not title:
+            flash("Title cannot be empty!", category='error')
+            return redirect(url_for("views.add"))
+        elif not author:
+            flash("Author cannot be empty!", category='error')
+            return redirect(url_for("views.add"))
+
+        new_offer = Offer(title=title, author=author, userid=userid)
+        db.session.add(new_offer)
+        db.session.commit()
+        flash("Offer added successfully!", category='success')
+
+        # Redirect back to homepage
+        return redirect(url_for("views.home"))
+    else:
+        return render_template("add.html", user=current_user)
 
