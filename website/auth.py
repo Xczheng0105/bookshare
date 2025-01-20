5b18fdb29bf062b0599133a5e5db4e943c0967d3
xczheng <zhengxc105@gmail.com>
2025-01-20 22:23:00 +0800

    Add some templates

--- website/auth.py
+++ website/auth.py
@@ -1,16 +1,34 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, request, redirect, flash
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
-    return "login page"
+    if request.method == 'POST':
+        login_data = request.form
+        
+    return render_template('login.html', user="hi")
 
 @auth.route('/logout')
 def logout():
     return "logs you out"
 
-@auth.route('/signup')
+@auth.route('/signup', methods=['GET', 'POST'])
 def signup():
-    return "signup page"
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        passwordC = request.form.get('passwordC')
+
+        if len(username) == 0:
+            flash("Username should not be empty!", category='error')
+        elif len(password) == 0:
+            flash("Password cannot be empty!", category='error')
+        elif password != passwordC:
+            flash("Passwords do not match!", category='error')
+        else:         
+            flash("Account created successfully!", category='success')
+        return render_template('signup.html', user="hi")
+    else:
+        return render_template('signup.html', user="hi")
 
