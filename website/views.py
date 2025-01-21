22e87579fb5605698716f7e60e9b096553ddac9b
xczheng <zhengxc105@gmail.com>
2025-01-21 10:06:22 +0800

    Implement more info feature

--- website/views.py
+++ website/views.py
@@ -6,11 +6,15 @@ from sqlalchemy import text
 
 views = Blueprint('views', __name__)
 
-@views.route('/', methods=['GET'])
+@views.route('/', methods=['GET', 'POST'])
 @login_required
 def home():
-    rows = db.session.execute(text('SELECT offer.title, offer.author, user.username FROM offer JOIN user ON user.id=offer.userid WHERE offer.userid != :uid'), {'uid': current_user.id})
-    return render_template("index.html", user=current_user, rows=rows)
+    if request.method == 'POST':
+        id = request.form.get("id")
+        return redirect(url_for('views.info', id=id))
+    else:
+        rows = db.session.execute(text('SELECT offer.id, offer.title, offer.author, user.username FROM offer JOIN user ON user.id=offer.userid WHERE offer.userid != :uid'), {'uid': current_user.id})
+        return render_template("index.html", user=current_user, rows=rows)
 
 @views.route('/add', methods=['GET', 'POST'])
 @login_required
@@ -37,4 +41,11 @@ def add():
         return redirect(url_for("views.home"))
     else:
         return render_template("add.html", user=current_user)
+    
+@views.route('/info', methods=['GET'])
+@login_required
+def info():
+    oid = int(request.args.get('id'))
+    query = db.session.execute(text('SELECT offer.title, offer.author, user.username, user.contactinfo FROM offer JOIN user ON user.id=offer.userid WHERE offer.id = :oid'), {'oid': oid}).first()
+    return render_template("info.html", user=current_user, query=query)
 
